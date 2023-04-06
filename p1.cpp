a1c76932ebf501212677d82d759c23b98e094cf8
adesh sengar <adeshsengar156@gmail.com>
2023-04-06 10:45:17 +0530

    second commit

--- p1.cpp
+++ p1.cpp
@@ -1,3 +1,5 @@
+
+// program to convert decimal to binary.
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -5,11 +7,11 @@ using namespace std;
 int main()
 {
     int n;  cin>>n;
-    int num=0 ,i;
+    int num=0 ,i=0;
     while(n!=0) {
-        int bit=n&1;
-        num=bit*pow(10,i)+num;
-        n=n>>1;
+        int bit=n & 1;
+        num=(bit*pow(10,i))+num;
+        n= n>>1;
         i++;
     }
     cout<<num;
